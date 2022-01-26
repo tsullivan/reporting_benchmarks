9b0e0b120898150f7b72eb617ef3999824ef84fd
Timothy Sullivan <tsullivan@elastic.co>
2022-01-26 11:53:41 -0700

    user non-interactive setup

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Test setup for benchmarking Dashboard PNG performance.
 
 Benchmark comparisons include the number of panels in the dashboard.
 
+
+## Run with Vagrant
+1. Run `yarn es snapshot` in the Kibana repo on the host machine. Note the snapshot version.
+2. Edit Vagrantfile to configure the memory size and number of CPUs
+3. Run `VERSION=8.1.0 vagrant up --provision`, matching the version with the ES snapshot instance.
+4. Run `vagrant ssh -c "/home/vagrant/setup.sh"`
--- setup/bootstrap_vagrant.sh
+++ setup/bootstrap_vagrant.sh
@@ -1,48 +1,53 @@
 #!/bin/sh
 set -e
-VERSION=8.1.0 # version of snapshot builds to download
+
 
 MANIFEST=$(curl --silent -XGET https://artifacts-api.elastic.co/v1/versions/$VERSION-SNAPSHOT/builds)
 BUILD_HASH=$(echo $MANIFEST | jq -r '.builds[0]')
 KBN_DOWNLOAD_URL=https://snapshots.elastic.co/$BUILD_HASH/downloads/kibana/kibana-$VERSION-SNAPSHOT-linux-x86_64.tar.gz
 MBT_DOWNLOAD_URL=https://snapshots.elastic.co/$BUILD_HASH/downloads/beats/metricbeat/metricbeat-$VERSION-SNAPSHOT-linux-x86_64.tar.gz
 
-cat << SCRIPT > /etc/profile.d/custom.sh
-VERSION=$VERSION
-BUILD_HASH=$BUILD_HASH
-KBN_DOWNLOAD_URL=$KBN_DOWNLOAD_URL
-MBT_DOWNLOAD_URL=$MBT_DOWNLOAD_URL
+echo "Bootstrapping for Kibana hash: ${BUILD_HASH}"
 
+cat << SCRIPT > /etc/profile.d/custom.sh
 echo
 echo \# Latest build: $BUILD_HASH
 echo \# Manifest updated: $(echo $MANIFEST | jq -r '.manifests["last-update-time"]')
 echo \# Latest download URL: $KBN_DOWNLOAD_URL
-echo \# Installation commands:
 echo
-echo \# Setup Kibana
-echo \    cd \$HOME
-echo \    wget --progress=bar:force:noscroll $KBN_DOWNLOAD_URL
-echo \    tar xzf kibana-$VERSION-SNAPSHOT-linux-x86_64.tar.gz
-echo \    cp /vagrant/share/config/kibana.base.yml kibana-$VERSION-SNAPSHOT/config/kibana.yml
-echo \    cp /vagrant/share/config/node.options kibana-$VERSION-SNAPSHOT/config/node.options
-echo \    cp -r /vagrant/share/config/cert-bundle kibana-$VERSION-SNAPSHOT/config/
-echo
-echo \# Set passwords in kibana-$VERSION-SNAPSHOT/config/kibana.yml, then run:
-echo \    cd kibana-$VERSION-SNAPSHOT
-echo \    ./bin/kibana
-echo
-echo \# Setup Metricbeat
-echo \    cd \$HOME
-echo \    wget --progress=bar:force:noscroll $MBT_DOWNLOAD_URL
-echo \    tar xzf metricbeat-$VERSION-SNAPSHOT-linux-x86_64.tar.gz
-echo \    cp /vagrant/share/config/metricbeat.yml metricbeat-$VERSION-SNAPSHOT
-echo \    cd metricbeat-$VERSION-SNAPSHOT
-echo \    ./metricbeat modules enable linux system
-echo \    sudo chown root metricbeat.yml
-echo \    sudo chown root modules.d/system.yml
-echo \    sudo ./metricbeat -e
+echo \    Run the setup.sh script to get started.
 echo
 SCRIPT
+
+cat << SCRIPT > /home/vagrant/setup.sh
+set -o verbose
+cd /home/vagrant
+wget --progress=bar:force:noscroll $KBN_DOWNLOAD_URL
+tar xzf kibana-$VERSION-SNAPSHOT-linux-x86_64.tar.gz
+cd kibana-$VERSION-SNAPSHOT
+rm config/kibana.yml
+rm config/node.options
+cp -r /vagrant/setup/config/* ./config
+
+cd /home/vagrant
+wget --progress=bar:force:noscroll $MBT_DOWNLOAD_URL
+tar xzf metricbeat-$VERSION-SNAPSHOT-linux-x86_64.tar.gz
+cp /vagrant/setup/config/metricbeat.yml metricbeat-$VERSION-SNAPSHOT
+cd metricbeat-$VERSION-SNAPSHOT-linux-x86_64
+./metricbeat modules enable linux system
+sudo chown root metricbeat.yml
+sudo chown root modules.d/system.yml
+SCRIPT
+
+
+cat << SCRIPT > /home/vagrant/start.sh
+set -o verbose
+cd /home/vagrant/kibana-$VERSION-SNAPSHOT
+./bin/kibana
+SCRIPT
+
+chmod a+x /home/vagrant/setup.sh
+chmod a+x /home/vagrant/start.sh
 
 # Allow access to elasticsearch
 echo "10.0.2.2  elasticsearch" >> /etc/hosts
