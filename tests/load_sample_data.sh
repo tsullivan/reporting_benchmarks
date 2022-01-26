a6d3c567eb83e8f2b75cf7a08d2fce66d116e306
Timothy Sullivan <tsullivan@elastic.co>
2022-01-26 16:58:52 -0700

    polish

--- tests/load_sample_data.sh
+++ tests/load_sample_data.sh
@@ -10,7 +10,7 @@ node $ROOT/scripts/es_archiver $CONFIG load archives/sample_dataoids
 curl $TEST_KIBANA_URL/api/spaces/space \
   -H 'Content-Type: application/json' \
   -H 'kbn-xsrf: reporting-test' \
-  --data-raw '{"name":"reporting-test-fixtures","id":"reporting-test-fixtures","description":"This space contains variously sized dashboard for Reporting benchmark test running.","initials":"r","color":"#CA8EAE","disabledFeatures":["siem","securitySolutionCases","enterpriseSearch","advancedSettings","indexPatterns","savedObjectsTagging","osquery","actions","stackAlerts","fleet"],"imageUrl":""}'
+  --data-raw '{"name":"reporting-test-fixtures","id":"reporting-test-fixtures","description":"This space contains variously sized dashboard for Reporting benchmark test running.","initials":"r","color":"#CA8EAE","disabledFeatures":["siem","securitySolutionCases","enterpriseSearch","advancedSettings","savedObjectsTagging","osquery","actions","stackAlerts","fleet"],"imageUrl":""}'
 echo
 
 curl -XPOST $TEST_KIBANA_URL/s/reporting-test-fixtures/api/saved_objects/_import?createNewCopies=false \
