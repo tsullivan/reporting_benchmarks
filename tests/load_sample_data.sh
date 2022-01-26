67613b5ed031c6bbd239caa32ba79c56a68998e6
Timothy Sullivan <tsullivan@elastic.co>
2022-01-26 16:43:35 -0700

    setup test data and saved objects

--- tests/load_sample_data.sh
+++ tests/load_sample_data.sh
@@ -7,8 +7,13 @@ ROOT=$HOME/elastic/kibana
 CONFIG="--config $ROOT/x-pack/test/functional/config.js"
 node $ROOT/scripts/es_archiver $CONFIG load archives/sample_dataoids
 
-curl -XPOST $TEST_KIBANA_URL/api/saved_objects/_import?createNewCopies=true \
+curl $TEST_KIBANA_URL/api/spaces/space \
+  -H 'Content-Type: application/json' \
+  -H 'kbn-xsrf: reporting-test' \
+  --data-raw '{"name":"reporting-test-fixtures","id":"reporting-test-fixtures","description":"This space contains variously sized dashboard for Reporting benchmark test running.","initials":"r","color":"#CA8EAE","disabledFeatures":["siem","securitySolutionCases","enterpriseSearch","advancedSettings","indexPatterns","savedObjectsTagging","osquery","actions","stackAlerts","fleet"],"imageUrl":""}'
+echo
+
+curl -XPOST $TEST_KIBANA_URL/s/reporting-test-fixtures/api/saved_objects/_import?createNewCopies=true \
   -H "kbn-xsrf: true" \
   --form file=@archives/sample_dataoids.ndjson
 echo
-
