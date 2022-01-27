199959d0874cd9dc1b50dfaed96a0c6a25df258b
Timothy Sullivan <tsullivan@elastic.co>
2022-01-26 17:27:03 -0700

    reorg

--- setup/setup_kibana.sh
+++ setup/setup_kibana.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
+set -o verbose
+
 export TEST_KIBANA_URL=http://elastic:changeme@localhost:5777
 export TEST_ES_URL=http://elastic:changeme@localhost:9200
 
+XSRF="kbn-xsrf: great-software"
+CONTENT_TYPE="Content-Type: application/json"
+
 curl $TEST_KIBANA_URL/api/license/start_trial \
-  -X 'POST' \
-  -H 'kbn-xsrf: great-software'
+  -H "$XSRF" -X "POST"
+echo
+
+curl $TEST_KIBANA_URL/api/saved_objects/index-pattern/kibana-system-event-log \
+  -H "$CONTENT_TYPE" -H "$XSRF" \
+  --data-raw '{"attributes":{"fieldAttrs":"{}","title":".kibana-event-log*","timeFieldName":"@timestamp","fields":"[]","typeMeta":"{}","runtimeFieldMap":"{}"}}'
+echo
 
 curl $TEST_KIBANA_URL/api/spaces/space \
-  -H 'Content-Type: application/json' \
-  -H 'kbn-xsrf: great-software' \
+  -H "$CONTENT_TYPE" -H "$XSRF" \
   --data-raw '{"name":"reporting-test-fixtures","id":"reporting-test-fixtures","description":"This space contains variously sized dashboard for Reporting benchmark test running.","initials":"r","color":"#CA8EAE","disabledFeatures":["siem","securitySolutionCases","enterpriseSearch","advancedSettings","savedObjectsTagging","osquery","actions","stackAlerts","fleet"],"imageUrl":""}'
 echo
 
 curl -XPOST $TEST_KIBANA_URL/s/reporting-test-fixtures/api/saved_objects/_import?createNewCopies=false \
-  -H "kbn-xsrf: true" \
-  --form file=@archives/sample_dataoids.ndjson
+  -H "$XSRF" --form file=@archives/sample_dataoids.ndjson
 echo
 
 ROOT=$HOME/elastic/kibana
