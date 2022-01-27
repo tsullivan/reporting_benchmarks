f36be923f6cf6fb37c1a37464698bcc7ad4b8d7d
Timothy Sullivan <tsullivan@elastic.co>
2022-01-26 20:35:56 -0700

    --wip-- [skip ci]

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -9,7 +9,7 @@ fi
 MANIFEST=$(curl --silent -XGET https://artifacts-api.elastic.co/v1/versions/$VERSION-SNAPSHOT/builds)
 BUILD_HASH=$(echo $MANIFEST | jq -r '.builds[0]')
 KBN_DOWNLOAD_URL=https://snapshots.elastic.co/$BUILD_HASH/downloads/kibana/kibana-$VERSION-SNAPSHOT-linux-x86_64.tar.gz
-MBT_DOWNLOAD_URL=https://snapshots.elastic.co/$BUILD_HASH/downloads/beats/metricbeat/metricbeat-$VERSION-SNAPSHOT-linux-x86_64.tar.gz
+MBT_DOWNLOAD_URL=https://snapshots.elastic.co/$BUILD_HASH/downloads/beats/metricbeat/metricbeat-$VERSION-SNAPSHOT-amd64.deb
 
 echo "Bootstrapping for Kibana hash: ${BUILD_HASH}"
 
@@ -37,27 +37,16 @@ rm config/kibana.yml
 rm config/node.options
 cp -r /vagrant/setup/config/* ./config
 KBN_INSTALL
+sudo -u vagrant /home/vagrant/setup-kibana.sh # install kibana as normal user
+
+# set up systemd services
+cp /vagrant/setup/kibana.service /etc/systemd/system/kibana.service
+
+/bin/systemctl daemon-reload
+/bin/systemctl start kibana
 
 cat << MBT_INSTALL > /home/vagrant/setup-metricbeat.sh
 set -o verbose
 cd /home/vagrant
 wget --progress=bar:force:noscroll $MBT_DOWNLOAD_URL
-tar xzf metricbeat-$VERSION-SNAPSHOT-linux-x86_64.tar.gz
-ln -s metricbeat-$VERSION-SNAPSHOT-linux-x86_64 metricbeat-SNAPSHOT
-cp /vagrant/metricbeat/metricbeat.yml metricbeat-SNAPSHOT
-(cd metricbeat-SNAPSHOT ; ./metricbeat modules enable linux system)
-chown root metricbeat-SNAPSHOT/metricbeat.yml
-chown root metricbeat-SNAPSHOT/modules.d/system.yml
 MBT_INSTALL
-chmod a+x /home/vagrant/setup-kibana.sh
-chmod a+x /home/vagrant/setup-metricbeat.sh
-
-sudo -u vagrant /home/vagrant/setup-kibana.sh # install kibana as normal user
-/home/vagrant/setup-metricbeat.sh # install metricbeat as root
-
-# set up systemd service
-cp /vagrant/setup/kibana.service /etc/systemd/system/kibana.service
-# cp /vagrant/setup/metricbeat.service /etc/systemd/system/metricbeat.service
-
-/bin/systemctl daemon-reload
-/bin/systemctl start kibana
